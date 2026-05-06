1a14f05d2cd89a915aea513bf47b829d6cfccee4
anilvainala <anil.vainala2@gmail.com>
2026-05-06 16:03:23 +0530

    updated HDFS path

--- job.py
+++ job.py
@@ -3,7 +3,9 @@ from pyspark.sql import SparkSession
 
 spark = SparkSession.builder.appName("Pipeline").enableHiveSupport().getOrCreate()
 
-df = spark.read.text("hdfs:///data/logs/access.log")
+##df = spark.read.text("hdfs:///data/logs/access.log")
+df = spark.read.text("hdfs://localhost:9000/data/logs/access.log")
+df.show(truncate=False)
 df.write.mode("overwrite").saveAsTable("logsdb.processed_logs")
 
 spark.stop()
